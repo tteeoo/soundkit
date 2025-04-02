c986a9a51e5bfb2ce01833a842e5e8ec75653754
Theo Henson <theodorehenson@protonmail.com>
2025-04-02 10:58:17 -0500

    Create  view module

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -std=c11 -g
 LDFLAGS = -ldl -lpthread -lm
+SDLFLAGS = $(shell sdl2-config --cflags --libs)
 
 TOOL_SOURCES = $(wildcard src/*.tool.c)
 TOOL_EXECS = $(TOOL_SOURCES:src/%.tool.c=build/sk.%)
@@ -9,10 +10,13 @@ TOOL_EXECS = $(TOOL_SOURCES:src/%.tool.c=build/sk.%)
 
 all: toolchain
 
-toolchain: $(TOOL_EXECS)
+toolchain: $(TOOL_EXECS) build/sk.view
 
 build/sk.fmsynth: src/cmads_modwave.c
 build/sk.playback build/sk.delay build/sk.lpf build/sk.hpf: src/cmads_stdins.c
+
+build/sk.view: src/view.tool.sdl.c | build
+	$(CC) $(CFLAGS) $(SDLFLAGS) $< -o $@ $(LDFLAGS)
 
 $(TOOL_EXECS): build/sk.% : src/%.tool.c | build
 	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS)
---
+++ src/view.tool.sdl.c
@@ -0,0 +1,78 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <SDL.h>
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "../miniaudio/miniaudio.h"
+
+#include "cmads_stdins.c"
+
+#define WIDTH        800
+#define HEIGHT       400
+#define MA_NO_DECODING
+#define MA_NO_ENCODING
+#define FORMAT       ma_format_f32
+#define CHANNELS     2
+#define SAMPLE_RATE  48000
+#define BATCH_SIZE   4000
+
+ma_result forward_data(cmads_stdins stdins, SDL_Renderer* renderer) {
+
+	float s[CHANNELS * BATCH_SIZE];
+	int running = 1;
+	while (running) {
+
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+		    if (event.type == SDL_QUIT) running = 0;
+		}
+
+		ma_data_source_read_pcm_frames((ma_data_source*)&stdins, s, BATCH_SIZE, NULL);
+		write(1, &s, BATCH_SIZE * sizeof(float) * CHANNELS);
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		// Currently only defaults to the first channel
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		for (int i = 0; i < BATCH_SIZE - 1; i++) {
+			int x1 = (i * WIDTH) / BATCH_SIZE;
+			int x2 = ((i+1) * WIDTH) / BATCH_SIZE;
+			int y1 = HEIGHT/2 + (s[i*CHANNELS] * HEIGHT/2);
+			int y2 = HEIGHT/2 + (s[(i+1)*CHANNELS] * HEIGHT/2);
+			SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+		}
+
+		SDL_RenderPresent(renderer);
+		sleep(BATCH_SIZE / SAMPLE_RATE);
+	}
+
+	return MA_SUCCESS;
+}
+
+int main(int argc, char** argv) {
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("soundkit view", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	cmads_stdins stdins;
+	cmads_stdins_config stdinsConfig;
+
+	stdinsConfig = cmads_stdins_config_init(FORMAT, CHANNELS, SAMPLE_RATE);
+	cmads_stdins_init(&stdinsConfig, &stdins);
+
+	forward_data(stdins, renderer);
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+    
+	cmads_stdins_uninit(&stdins);
+
+	(void)argc;
+	(void)argv;
+	return 0;
+}
