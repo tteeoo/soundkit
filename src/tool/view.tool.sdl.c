d688408d8efcec6915c62634cad125c9b448139b
Theo Henson <theodorehenson@protonmail.com>
2026-01-09 01:02:34 -0500

    Add view 0 line

--- src/tool/view.tool.sdl.c
+++ src/tool/view.tool.sdl.c
@@ -39,6 +39,8 @@ void render(SDL_Renderer* renderer) {
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderDrawLine(renderer, 0, HEIGHT/2, WIDTH, HEIGHT/2);
 		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 
 		frameCount = BATCH_SIZE;
